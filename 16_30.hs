75471a61becd954c2cbe077f42b58a653be12fd3
Jakub Batko <bosswell9@gmail.com>
2024-07-13 22:20:00 +0200

    Solve problem 16

--- 16_30.hs
+++ 16_30.hs
@@ -1,1 +1,10 @@
 -- Problem 16
+
+dropEvery :: [a] -> Int -> [a]
+dropEvery list moduloIndex = fn list moduloIndex 1
+    where
+        fn [] _ _ = []
+        fn (x:xs) moduloIndex currentIndex = [x | 0 /= (currentIndex `mod` moduloIndex)] ++ fn xs moduloIndex (currentIndex + 1)
+
+main :: IO ()
+main = print (dropEvery [1, 2, 3, 4, 5, 6, 7] 4)
